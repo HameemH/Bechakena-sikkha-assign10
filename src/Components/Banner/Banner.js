29d17426e5092c4020668fdf766131f2cfdef241
HameemH <hameemhossain2@gmail.com>
2022-04-16 23:29:12 +0600

    navbar and banner created

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import './Banner.css';
+
+const Banner = () => {
+    return (
+        <div>
+            <Carousel className=' banner '>
+  <Carousel.Item>
+    <img
+      className="d-block w-100 h-50"
+      src="https://i.ibb.co/tmXfcwW/service1-1.jpg"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://i.ibb.co/9tcHr0Q/service2-1.jpg"
+      alt="Second slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://i.ibb.co/KGxVvRG/service3-1.jpg"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+        </div>
+    );
+};
+
+export default Banner;
